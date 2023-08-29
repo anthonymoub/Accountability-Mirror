d56481f1c47d195daef7ac54d8d4d54e0645d186
Anthony <am4315@georgetown.edu>
2023-08-28 23:39:32 -0400

    SQL Easy day 7

---
+++ SQL/Easy/Day7.sql
@@ -0,0 +1,8 @@
+-- link: https://datalemur.com/questions/cards-issued-difference
+
+-- solution:
+
+select card_name, max(issued_amount) - min(issued_amount) as difference
+from monthly_cards_issued
+group by card_name
+order by difference desc
